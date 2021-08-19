a1afef7d420f9483ee5b18bf725fee10331c34b2
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-19 18:55:15 +0530

    Add project files.

---
+++ DataStructuresAndAlgorithmsPracticeProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithmsPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter \n1. Permutations of a String" +
+                                    "\n2. Binary Search A Word");
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    PermutationsOfAString.StringInput();
+                    break;
+
+                case 2:
+                    BinarySearchAWord.StringInput();
+                    break;
+            }
+        }
+    }
+}
